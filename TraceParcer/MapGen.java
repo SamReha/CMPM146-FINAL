b147283e90c13e8c26dd45145b646bbb1fc99e00
unknown <nijowill@ucsc.edu>
2015-06-08 13:30:33 -0700

    Parcing Done, needs to be integraded. no big deal.

--- TraceParcer/MapGen.java
+++ TraceParcer/MapGen.java
@@ -10,28 +10,11 @@ public class MapGen {
       ReadFile file = new ReadFile(filename);
       String[] Trace = file.OpenFile();
       for (int i = 0; i < Trace.length; i++){
-        //System.out.println(Trace[i]);
-        String type = Trace[i];
-        switch (type) {
-          case "room start" : // build room, place player
-                              System.out.println("Start");
-                              break;
-          case "room keylock" : //build room, place door, place key
-                                System.out.println("keylock");
-                                break;
-          case "room maze" : //build room, place maze
-                             System.out.println("maze");
-                             break;
-          case "room no_obstacle" : //build room
-                                    System.out.println("no obstacle");
-                                    break;
-          case "room end" : //build room
-                            System.out.println("End");
-                            break;
-        }
+        file.Excecute(Trace, i);
+
       } 
-    } catch(ArrayIndexOutOfBoundsException e) {
-      System.err.println("No File");
+    //} catch(ArrayIndexOutOfBoundsException e) {
+     // System.err.println("No File");
     } catch(IOException e) {
       System.err.println(e.getMessage());
     }
